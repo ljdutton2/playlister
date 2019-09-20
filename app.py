b9bd7933e57ce7bbe199189557dbf7f8f801031e
lauren <ljdutton2@icloud.com>
2019-09-19 23:17:49 -0700

    initiated

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,jsonify
 app = Flask(__name__)
 
-@approute('/')
+@app.route('/')
 def index():
     return render_template
 
+@app.route('/playlists')
+def playlists_index():
+    """Show all playlists."""
+
+
+
 if __name__ == '__main__':
-    
+    app.debug = True
